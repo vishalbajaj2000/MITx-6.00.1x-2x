a1954bc7f307f4f2695adf230244de369899fc56
Vishal Bajaj <vishalbajaj2000@gmail.com>
2019-09-08 11:17:25 -0400

    Problem set 3 updated and completed

--- Week1-Problem-set-3.py
+++ Week1-Problem-set-3.py
@@ -5,25 +5,24 @@ Write a program that prints the longest substring of s in which the letters occu
 
 Longest substring in alphabetical order is: beggh
 '''
-#Not working!
-s = 'azcbobobegghakl'
+s = 'ootocrklojrzrmjrdqh'
 wordlist = []
+wordlen = []
 for i in range(len(s)):
     word = ''
     j=i
-    if i+1 == len(s):
-        wordlist.append(s[i])
-    elif s[i] > s[i+1]:
-        wordlist.append(s[i])
+    if i+1 == len(s) or s[i] > s[i+1]:
+        word = s[i]
+        wordlist.append(word)
+        wordlen.append(len(word))
     else:
-        lastletter = False
         while s[j] <= s[j+1]:
             word = word + (s[j])
             j += 1
             if j+1 >= len(s):
                 break
-            
-        word = word + (s[j-1])
-        print(word)
+        word = word + (s[j])
         wordlist.append(word)
-print(wordlist)
+        wordlen.append(len(word))
+print('Longest substring in alphabetical order is: ' + wordlist[wordlen.index(max(wordlen))])
+#Mote: using a dicts and getting max index from dictionary key values did not work in edx grader!!
