57ab2bf0c5197e8455d7b13330dff6ccf9adad99
Vishal Bajaj <vishalbajaj2000@gmail.com>
2019-09-16 00:42:01 -0400

    completed week 2problems 1-2-3

--- Scratchpad.py
+++ Scratchpad.py
@@ -1,15 +1,19 @@
-x = 23
-epsilon = 0.01
-step = 0.1
-guess = 0.0
+monthlyInterestRate = annualInterestRate/12
+l = 0
+h = (balance * (1+monthlyInterestRate)**12) / 12
+if annualInterestRate == 0:
+    print('Lowest Payment: ' + str(round(balance/12,2)))
+while True:
+    tempBalance = balance
+    fixPay = ((l + h) / 2)
+    for i in range(1,13):
+        monthlyUnpaidBlance = tempBalance - fixPay
+        tempBalance = monthlyUnpaidBlance + (monthlyInterestRate * monthlyUnpaidBlance)
 
-while abs(guess**2-x) >= epsilon:
-    if guess <= x:
-        guess += step
-    else:
+    if abs(tempBalance) < 0.01:
+        print('Lowest Payment: ' + str(round(fixPay,2)))
         break
-
-if abs(guess**2 - x) >= epsilon:
-    print('failed')
-else:
-    print('succeeded: ' + str(guess))
+    elif tempBalance > 0:
+        l = fixPay
+    elif tempBalance < 0:
+        h = fixPay  
