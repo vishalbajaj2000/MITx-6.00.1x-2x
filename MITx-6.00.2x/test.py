54b2e5687c2bf46ad54aa954e6508f5362c6b2eb
Vishal Bajaj <vishalbajaj2000@gmail.com>
2019-10-31 13:12:12 -0400

    new comit

---
+++ MITx-6.00.2x/test.py
@@ -0,0 +1,14 @@
+class Dummy(object):
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+    
+    def geta(self):
+        return self.a
+    
+    def getb(self):
+        return self.b
+
+    def getc(self):
+        return self.c
