a6613078860b7307345e61427402e17a2f4597a1
Vishal Bajaj <vishalbajaj2000@gmail.com>
2019-11-07 13:30:19 -0500

    completed pset1 2x

--- MITx-6.00.2x/pset1/ps1.py
+++ MITx-6.00.2x/pset1/ps1.py
@@ -1,12 +1,36 @@
 ###########################
 # 6.00.2x Problem Set 1: Space Cows 
 
-from ps1_partition import get_partitions
+#from ps1_partition import get_partitions
 import time
+#From codereview.stackexchange.com                    
+def partitions(set_):
+    if not set_:
+        yield []
+        return
+    for i in range(2**len(set_)//2):
+        parts = [set(), set()]
+        for item in set_:
+            parts[i&1].add(item)
+            i >>= 1
+        for b in partitions(parts[1]):
+            yield [parts[0]]+b
 
-#================================
+
+# This is a helper function that will fetch all of the available 
+# partitions for you to use for your brute force algorithm.
+def get_partitions(set_):
+    for partition in partitions(set_):
+        yield [list(elt) for elt in partition]
+
+### Uncomment the following code  and run this file
+### to see what get_partitions does if you want to visualize it:
+
+# for item in (get_partitions(['a','b','c','d'])):
+#     print(item)
+# ================================
 # Part A: Transporting Space Cows
-#================================
+# ================================
 
 def load_cows(filename):
     """
@@ -54,21 +78,40 @@ def greedy_cow_transport(cows,limit=10):
     transported on a particular trip and the overall list containing all the
     trips
     """
-    cow_temp = cows.copy()
-    cow_names = list(cow_temp.keys())
-    cow_weights = list(cow_temp.values())
+
+    cows_temp = []
+    ct = cows.copy()
+    cow_weights = sorted(list(cows.values()), reverse=True)
+    for i in cow_weights:
+        for k,v in ct.items():
+            if i == ct[k]:
+                cows_temp.append((k,v))
+                ct[k] = 'Used'
+            else:
+                pass
     trips = []
     flag = True
+    used = []
     while flag:
-        cow_weights_max = max(cow_weights)
-        cw_max_ind = cow_weights.index(cow_weights_max)
-        cw_nm_max = cow_names[cw_max_ind]
-        for i in len(cow_temp):
-            trips[i].append([cw_nm_max])
-
-
-
-
+        available = limit
+        trips.append([])
+        for i in cows_temp:
+            if i[1] <= available:
+                trips[-1].append(i[0])
+                used.append(i)
+                available = available - i[1]
+                if available == 0:
+                    break
+            else:
+                pass
+        for i in used:
+            if i in cows_temp:
+                cows_temp.pop(cows_temp.index(i))
+            else:
+                pass
+        if len(cows_temp) == 0:
+            flag = False
+    return trips
 
 # Problem 2
 def brute_force_cow_transport(cows,limit=10):
@@ -91,8 +134,30 @@ def brute_force_cow_transport(cows,limit=10):
     transported on a particular trip and the overall list containing all the
     trips
     """
-    # TODO: Your code here
-    pass
+    ct =  cows.copy()
+    ctp = []
+    for i in get_partitions(ct):
+        ctp.append(i)
+    accepted = []
+    for i,l in enumerate(ctp):
+        combo_ok = True
+        for j, sl in enumerate(l):
+            tripsum = 0
+            for k, ssl in enumerate(sl):
+                tripsum = tripsum + ct[ssl]
+            if tripsum > limit:
+                combo_ok = False
+                break
+            else:
+                pass  
+        if combo_ok == True:
+            accepted.append(l)
+
+    accp_len = []
+    for i in accepted:
+        accp_len.append(len(i))
+                    
+    return accepted[accp_len.index(min(accp_len))]
 
         
 # Problem 3
@@ -119,11 +184,23 @@ Do not submit this along with any of your answers. Uncomment the last two
 lines to print the result of your problem.
 """
 
-cows = load_cows("ps1_cow_data.txt")
+cows = load_cows("C:\\Users\\vbajaj\\Local Files\\Dev\\MITx-6.00.1x-2x\\MITx-6.00.2x\\pset1\\ps1_cow_data.txt")
 limit=100
-print(cows)
+#print(cows)
 
-print(greedy_cow_transport(cows, limit))
-print(brute_force_cow_transport(cows, limit))
+# print(greedy_cow_transport(cows, limit))
+# print(greedy_cow_transport({'Muscles': 65, 'Miss Bella': 15, 'MooMoo': 85, 'Lotus': 10, 'Louis': 45, 'Horns': 50, 'Clover': 5, 'Polaris': 20, 'Milkshake': 75, 'Patches': 60}, 100))
+
+# print(greedy_cow_transport({'Abby': 28, 'Rose': 42, 'Starlight': 54, 'Betsy': 39, 'Luna': 41, 'Buttercup': 11, 'Coco': 59, 'Willow': 59}, 120))
+# print(greedy_cow_transport({'Lilly': 24, 'Abby': 38, 'Dottie': 85, 'Rose': 50, 'Betsy': 65, 'Patches': 12, 'Daisy': 50, 'Buttercup': 72, 'Coco': 10, 'Willow': 35}, 100))
+
+start = time.time()
+print(greedy_cow_transport(cows, 10))
+end = time.time()
+print(end - start)
 
 
+start = time.time()
+print(brute_force_cow_transport(cows, 10))
+end = time.time()
+print(end - start)
